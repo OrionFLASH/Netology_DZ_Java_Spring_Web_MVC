95e59e6dab8ed98f2be49b8b1c2a0736862bc6a5
OrionFlash <orionflash@example.com>
2026-05-24 20:24:08 +0300

    Добавлен PostController по спецификации лекции
    REST-эндпоинты /api/posts реализованы с делегированием в PostService.

---
+++ src/main/java/ru/netology/mvc/controller/PostController.java
@@ -0,0 +1,49 @@
+package ru.netology.mvc.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.mvc.model.Post;
+import ru.netology.mvc.service.PostService;
+
+import java.util.List;
+
+/**
+ * REST-контроллер для управления постами.
+ * <p>
+ * Структура и сигнатуры методов соответствуют лекции Netology «Spring Web MVC».
+ * Вся бизнес-логика вынесена в {@link PostService}.
+ */
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private final PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Post> all() {
+        return service.all();
+    }
+
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
+        service.removeById(id);
+    }
+}
